70eeb04b13f2ccc3d8fe0a6dde0529eea654593c
brian <brian@DESKTOP-PJTH022.fios-router.home>
2016-09-23 23:19:22 -0400

    updates

--- Class Project/src/edu/buffalo/cse116/Card.java
+++ Class Project/src/edu/buffalo/cse116/Card.java
@@ -2,79 +2,64 @@ package edu.buffalo.cse116;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Card {
 	
-	//first, create three different groups that will represent what kinds of cards we'll be dealing with
+private String name;
 
-	//the weapons of the game
-	public enum Weapons{
-		KNIFE,ROPE,REVOLVER,WRENCH,PIPE,CANDLESTICK;
-	}
-	
-	//the game's potential suspects
-	public enum Suspects{
-		MISSSCARLET,MRSWHITE,PROFESSORPLUM,COLONELMUSTARD,MRGREEN,MRSPEACOCK;
-	}
-	
-	//the rooms that are in the game
-	public enum Rooms{
-		KITCHEN, BALLROOM, CONSERVATORY, DININGROOM, SAFE, BILLARDROOM, LIBRARY, LOUNGE, HALL, STUDY;
-	}
+public enum CardType{
+	PERSON,WEAPON,ROOM;
+}
+private CardType type;
 
-	//this variable will represent the card's absolute type for use thruought the game
-	final Suspects suspects;
-	final Rooms rooms;
-	final Weapons weapons;
-	
-	//the constructor will assign a card to a type permanently
-	
-	private Card(final Weapons weapons,final Suspects suspects, final Rooms rooms)
-	{
-		this.weapons=weapons;
-		this.rooms=rooms;
-		this.suspects=suspects;
+public CardType getCardType(){
+	return type;
+}
 
-	}
-	
-	//here we'll begin creating the deck of cards, which will need to have one card from each group removed eventually to make the solutions group, 
-	//the rest will have to be distributed evenly among the players
-	
-	//i chose an arraylist since the order of elements isn't relevant and i'll probably be manipulating the elements inside it often
-	private static final List<Card>Test=new ArrayList<Card>();
-	
-	
-	//three for each loops that will loop thru the enum elements of the respected cardtype, 
-	//it will insert the element into 
-	static{
-		for (Weapons weapons:Weapons.values()){
-			for(Suspects suspects:Suspects.values()){
-				for(Rooms rooms:Rooms.values()){
-				Test.add(new Card(weapons,suspects,rooms));
-				}
-			}
-		}
-	}
-	
-	public static ArrayList<Card>initialDeck(){
-		return new ArrayList<Card>(Test);
-	}
-	
-	
-	//public ArrayList<Card>SolutionSet();
-	
-	public static ArrayList<Card>deal(final List<Card>deck,final int n){
-		final int deckSize=deck.size();
-		final List<Card>visibleHand=deck.subList(deckSize-n, deckSize);
-		final ArrayList<Card>hand=new ArrayList<Card>(visibleHand);
-		visibleHand.clear();
-		return hand;
-	}
+public void setCardType(CardType cardType){
+	this.type=cardType;
+}
 
-	//cards are grouped by type
-	
-	//at start, 3 cards are randomly chosen, one from each group
-	//these will be the solution set of cards
-	
-	//depending on number of players, deal out remainder of cards
+public String getName(){
+	return name;
+}
+
+public void setName(String name){
+	this.name=name;
+}
+
+public Card(String name,CardType type){
+	super();
+	this.name=name;
+	this.type=type;
+}
+
+
+@Override
+public int hashCode(){
+	final int prime=31;
+	int result=1;
+	result=prime*result+((name==null)?0:name.hashCode());
+	result=prime*result+((type==null)?0:name.hashCode());
+	return result;
+}
+
+@Override
+public boolean equals(Object obj){
+	if(this==obj)
+		return true;
+	if(obj==null)
+		return false;
+	if(getClass()!=obj.getClass())
+		return false;
+	Card other=(Card)obj;
+	if(name==null){
+		if(other.name!=null)
+			return false;
+	}else if(!name.equals(other.name))
+		return false;
+ if(type !=other.type)
+	 return false;
+ return true;}
 }
--- Class Project/src/edu/buffalo/cse116/CardTests.java
+++ Class Project/src/edu/buffalo/cse116/CardTests.java
@@ -1,9 +1,24 @@
 package edu.buffalo.cse116;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import edu.buffalo.cse116.Card.CardType;
 
 public class CardTests {
 	
+	@Test
+	public void getCardName(){
+		Card card=new Card("COLONEL_MUSTARD",CardType.PERSON);
+		assertEquals(card.getName(),"COLONEL_MUSTARD");
+	}
+	
+	@Test
+	public void getCardType(){
+		Card card=new Card("LIBRARY",CardType.ROOM);
+		assertEquals(card.getCardType(),CardType.ROOM);
+	}
 
 	
 		
